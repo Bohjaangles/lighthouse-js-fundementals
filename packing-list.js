f0ad675cc302f85160af410eea17c3c93c0479e1
Russel Mueller <bohjaangles@gmail.com>
2022-09-21 22:09:43 +0000

    upadted packing list to use a while instead of a for loop

---
+++ packing-list.js
@@ -0,0 +1,8 @@
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("Kitchen stuff to pack:");
+counter = 0;
+while (counter < packingList.length){
+  console.log(packingList[counter]);
+  counter++;
+}
